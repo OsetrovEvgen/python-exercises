3ac3c1c7fb59a5dce916b8f23adf8cdae1175dd4
Osetrov E <rabota87890012@gmail.com>
2022-06-24 21:35:12 +0300

    PB-3 task fix

--- basics4/difference_integer.py
+++ basics4/difference_integer.py
@@ -1,2 +1,12 @@
+def dif_int(a):
+    if a.isnumeric() == False:
+        print('Enter a valid number')
+    x = ''.join(sorted(a))
+    y = ''.join(sorted(a, reverse=True))
+    x = int(x)
+    y = int(y)
+    z = y - x
+    return z
 
 
+print(dif_int(input()))
